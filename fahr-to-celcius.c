77b58b472d049dab0ef2c25fe855a0088437cd3f
zawkindev <zawkindev@gmail.com>
2024-03-19 13:16:52 +0500

    refactor

--- fahr-to-celcius.c
+++ fahr-to-celcius.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
 main(){
-  float fahr, celcius;
-  float lower, upper, step;
+  float fahr;
 
-  lower = 0;
-  upper = 300;
-  
-  for(fahr=0; fahr<=upper; fahr+=20){
-    fahr = fahr + step;   
-    celcius = (5.0 / 9.0) * (fahr - 32);
-    printf("%3.1f\t%6.2f\n", fahr, celcius);
+  for(fahr=0; fahr<=300; fahr+=22){
+    printf("%3.1f\t%6.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
   }
 }
 
