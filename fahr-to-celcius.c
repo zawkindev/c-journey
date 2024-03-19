f353cc74b74715a3875f5e23bdf008dbc242f8f8
zawkindev <zawkindev@gmail.com>
2024-03-19 13:04:50 +0500

    feat: use for instead of while

--- fahr-to-celcius.c
+++ fahr-to-celcius.c
@@ -6,12 +6,11 @@ main(){
 
   lower = 0;
   upper = 300;
-  step = 22;  
   
-  fahr = lower;
-  while(fahr <= upper){
+  for(fahr=0; fahr<=upper; fahr+=20){
     fahr = fahr + step;   
     celcius = (5.0 / 9.0) * (fahr - 32);
     printf("%3.1f\t%6.2f\n", fahr, celcius);
   }
 }
+
