b9e1a4544dbdee34e158734aa877dc89bdcfc643
zawkindev <zawkindev@gmail.com>
2024-03-21 06:05:38 +0500

    feat: add symbolic contants

--- fahr-to-celcius.c
+++ fahr-to-celcius.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define HIGHER 300 
+#define STEP 22
+
+
 main(){
   float fahr;
 
-  for(fahr=300; fahr>=0; fahr-=22){
+  for(fahr=HIGHER; fahr>=LOWER; fahr-=STEP){
     printf("%3.1f\t%6.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
   }
 }
