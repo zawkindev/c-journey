ac6178e55228f0a710f0e17aee2b84006aa3354f
zawkindev <zawkindev@gmail.com>
2024-03-22 10:04:57 +0500

    feat: wc of linux

---
+++ wc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main() {
+  int c, nc, nw, nl, state;
+
+  nw = nl = 0;
+  for (nc = 0; (c = getchar()) != EOF; nc++) {
+    if (c == '\n')
+      ++nl;
+
+    if (c == ' ' || c == '\t' || c == '\n' || c == EOF) {
+      if (state == IN)
+        ++nw;
+      state = OUT;
+    }
+
+    else {
+      state = IN;
+    }
+  }
+
+  printf("lines: %d; words: %d; chars: %d\n", nl, nw, nc);
+}
