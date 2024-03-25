f039595fa1a7811ca9af713123cf7d3e4b115cd4
shahruz <shahruz@shahruzs-MacBook-Pro.local>
2024-03-25 14:18:11 +0500

    feat: added convert function

--- fahr_to_celcius.c
+++ fahr_to_celcius.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 #define LOWER 0
-#define HIGHER 300 
+#define HIGHER 300
 #define STEP 22
 
+float convert(float fahr) { return (5.0 / 9.0) * (fahr - 32); }
 
-main(){
+main() {
   float fahr;
 
-  for(fahr=HIGHER; fahr>=LOWER; fahr-=STEP){
-    printf("%3.1f\t%6.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+  for (fahr = HIGHER; fahr >= LOWER; fahr -= STEP) {
+    printf("%3.1f\t%6.2f\n", fahr, convert(fahr));
   }
 }
-
