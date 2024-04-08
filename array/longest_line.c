67797fdf6e74d2c928d52443005a050120408f3d
zawkindev <zawkindev@gmail.com>
2024-04-08 16:33:39 +0500

    feat: add reverse function

--- array/longest_line.c
+++ array/longest_line.c
@@ -4,12 +4,14 @@
 
 int readline(char line[]);
 void copy(char to[], char from[]);
+void reverse(char line[], char reversed[], int limit);
 
 int main() {
   int cur,           // current line length
       max;           // max line length
   char cl[MAX_LINE], // current line
-      ml[MAX_LINE];  // max line
+      ml[MAX_LINE],  // max line
+      rl[MAX_LINE];  // reversed line
 
   max = 0;
   while ((cur = readline(cl)) > 0) {
@@ -19,7 +21,9 @@ int main() {
     }
   }
 
-  printf("\nlongest line: %s\n", ml);
+  reverse(rl, ml, max);
+  printf("\n\nlongest line: %s", ml);
+  printf("\nreverse of it: %s\n", rl);
 
   return 0;
 }
@@ -42,5 +46,16 @@ void copy(char max_line[], char current_line[]) {
   i = 0;
   while ((max_line[i] = current_line[i]) != '\0') {
     ++i;
+  }
+}
+
+void reverse(char reversed[], char line[], int limit) {
+  int i, j;
+
+  i = limit-1;
+  j = 0;
+  while ((reversed[j] = line[i]) != EOF && i > -1 && j < limit) {
+    --i;
+    ++j;
   }
 }
