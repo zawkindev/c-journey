6cbc6d0df914a2bf4409a380a42845944d8a486b
zawkindev <zawkindev@gmail.com>
2024-08-01 20:46:47 +0500

    feat: pointer arithmetics

--- pointer.c
+++ pointer.c
@@ -1,19 +1,24 @@
 #include "stdio.h"
 
-void swap(void *a, void *b){
+void swap(void *a, void *b) {
   void *temp = a;
-  a=b;
-  b=temp;
+  a = b;
+  b = temp;
 }
 
 int main() {
   int a = 2;
   float b = 1.0;
 
-  printf("a=%d, b=%f\n",a,b);
-  swap(&a,&b);
+  printf("a=%d, b=%f\n", a, b);
+  swap(&a, &b);
   printf("-------Swapped------\n");
-  printf("a=%f, b=%d\n",a,b);
+  printf("a=%f, b=%d\n", a, b);
+
+  float arr[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
+  for (int i = 0; i < 5; i++) {
+    printf("%.1f ", *(arr + i));
+  }
 
   return 0;
 }
