3d373b568aa8d42d4d5e3cfe043d54ce65796603
zawkindev <zawkindev@gmail.com>
2024-11-04 10:31:41 +0500

    feat: buble sort

--- algorithms/sort.c
+++ algorithms/sort.c
@@ -9,6 +9,7 @@ void swap(int a, int b, int arr[]) {
 void selection_sort(int arr[], int size) {
   for (int i = 0; i < size; i++) {
     int min_index = i;
+
     for (int j = i + 1; j < size; j++) {
       int min = arr[min_index];
       int current = arr[j];
@@ -21,6 +22,17 @@ void selection_sort(int arr[], int size) {
   }
 }
 
+void buble_sort(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size - 1; j++) {
+      int c = arr[j];     // current
+      int n = arr[j + 1]; // next
+      if (c > n) {
+        swap(j, j + 1, arr);
+      }
+    }
+  }
+}
 
 int main() {
   int arr[] = {64, 25, 12, 22, 11};
@@ -32,7 +44,7 @@ int main() {
   }
   printf("\n");
 
-  selection_sort(arr, size);
+  buble_sort(arr, size);
 
   printf("Sorted array: ");
   for (int i = 0; i < size; i++) {
