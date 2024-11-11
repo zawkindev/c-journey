f3f38ae9a2ea613a52f6a02cce6b31261f6e97a8
zawkindev <zawkindev@gmail.com>
2024-11-11 11:43:27 +0500

    feat: linked list

---
+++ data-structures/linked_list.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+struct Node {
+  int data;
+  struct Node *next;
+};
+
+struct Node *createNode(int data) {
+  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  return newNode;
+}
